795454298696981eb5eef7a93ba497c53f2394f3
HORINOUCHI Masato <thermes@confei.to>
2016-03-29 15:56:32 +0900

    refactoring.

--- poh7/megane.rb
+++ poh7/megane.rb
@@ -8,25 +8,21 @@ m = gets.chomp.to_i
 p_ = []
 m.times { p_ << gets.chomp.split(' ').map(&:to_i) }
 
-catch :q_loop_break do
+catch :loop_break do
   (n - m + 1).times do |q_y|
     (n - m + 1).times do |q_x|
       match = true
 
-      catch :p_loop_break do
-        m.times do |j|
-          m.times do |i|
-            if q_[q_y + j][q_x + i] != p_[j][i]
-              match = false
-              throw :p_loop_break
-            end
-          end
+      m.times do |j|
+        if q_[q_y + j][q_x, m] != p_[j]
+          match = false
+          break
         end
       end
 
       if match
         puts "#{q_y} #{q_x}"
-        throw :q_loop_break
+        throw :loop_break
       end
     end
   end
