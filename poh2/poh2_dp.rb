1633b65e7518fc42c244568eb352302ca9303b53
HORINOUCHI Masato <thermes@confei.to>
2020-05-17 14:21:51 +0900

    poh2 memo2.

--- poh2/poh2_dp.rb
+++ poh2/poh2_dp.rb
@@ -9,35 +9,50 @@ h.times.each do
   dp << Array.new(w) {Array.new(w+1)}
 end
 
+table = []
+(300+1).times do
+  table << Array.new(301)
+end
+table[1][1] = ds.flatten.filter {|v| v == 0}.size
+
 gets
 ARGF.each do |line|
   (s, t) = line.chomp.split(' ').map(&:to_i)
 
-  count = 0
-  (0..h-s).each do |y|
-    (0..w-t).each do |x|
-      if dp[y][x][t] == false
-        next
-      end
-      if ds[y][x] == 1
-        dp[y][x][t] = false
-        next
-      end
-
-      flag = true
-      s.times do |j|
-        next if dp[y+j][x][t]
-        if ds[y+j][x..(x+t-1)].sum >= 1
-          dp[y+j][x][t] = false
-          flag = false
-          break
+  if !table[s][t].nil? && table[s][t] >= 0
+    count = table[s][t]
+  else
+    count = 0
+    (0..h-s).each do |y|
+      (0..w-t).each do |x|
+        if dp[y][x][t] == false
+          next
         end
-        dp[y+j][x][t] = true
-      end
+        if ds[y][x] == 1
+          dp[y][x][t] = false
+          next
+        end
 
-      count += 1 if flag
+        flag = true
+        s.times do |j|
+          next if dp[y+j][x][t]
+          if dp[y+j][x][t] == false
+            flag = false
+            break
+          end
+          if ds[y+j][x..(x+t-1)].sum >= 1
+            dp[y+j][x][t..-1] = false
+            flag = false
+            break
+          end
+          dp[y+j][x][t] = true
+        end
+
+        count += 1 if flag
+      end
     end
   end
+  table[s][t] = count if table[s][t].nil?
 
   puts count
 end
